4ce22b3f1154236d9eae2ea154dc8d6777bad720
Aimen <aimen.moten@gmail.com>
2023-07-20 19:19:14 +0500

    footer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Navbar'
 import "./styles.css"
 import Form from './Form'
+import Footer from './Footer'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Form/>
+      <Footer/>
     </div>  
   )
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        Made with laughter and love by Aimen Moten 
+    </footer>
+  )
+}
+
+export default Footer
